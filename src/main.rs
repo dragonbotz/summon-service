5858e8858b72af0bbd12a9202113f7b29dccd2c3
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2023-08-14 13:02:55 +0200

    Adds web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+//! Dragon Bot Z's summon service
+//!
+//! This is the entry point of the summon service
+//!
+//! Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
+
+use actix_web::{HttpServer, App};
+use log::{error, info};
+
+#[actix_web::main]
+async fn main() {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let server = HttpServer::new(move || {
+        App::new()
+    })
+        .bind(("0.0.0.0", 8083));
+
+    if let Err(error) = server {
+        error!("An error occured while setting up web server: {error}");
+        std::process::exit(1);
+    }
+    let server = server.unwrap();
+    
+    info!("Starting web server: http://0.0.0.0:8083");
+    if let Err(error) = server.run().await {
+        error!("An error occured while starting up web server: {error}");
+        std::process::exit(1);
+    }
 }
