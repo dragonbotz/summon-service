9f4a8f970aba12d3147c65ac977b78d1e38341bb
Lahène Belhadi <lahcene.belhadi@gmail.com>
2023-10-01 16:14:08 +0200

    fix: Adds summon route

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@
 //! Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
 pub mod core;
 
-use actix_web::{HttpServer, App};
+use actix_web::{web, App, HttpServer};
 use log::{error, info};
+use reqwest;
 
 use crate::core::api;
 
@@ -15,18 +16,21 @@ async fn main() {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
+    let http_client = reqwest::Client::new();
     let server = HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(http_client.clone()))
             .service(api::root)
+            .service(api::summon)
     })
-        .bind(("0.0.0.0", 8083));
+    .bind(("0.0.0.0", 8083));
 
     if let Err(error) = server {
         error!("An error occured while setting up web server: {error}");
         std::process::exit(1);
     }
     let server = server.unwrap();
-    
+
     info!("Starting web server: http://0.0.0.0:8083");
     if let Err(error) = server.run().await {
         error!("An error occured while starting up web server: {error}");
