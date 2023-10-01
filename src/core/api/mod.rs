fd672c7a5b58185419cc4e6d4be65f7168416690
Lahène Belhadi <lahcene.belhadi@gmail.com>
2023-10-01 16:14:00 +0200

    fix: Adds summon route

--- src/core/api/mod.rs
+++ src/core/api/mod.rs
@@ -1,9 +1,23 @@
 //! This module contains the service's api implementation
 //!
 //! Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
-use actix_web::{Responder, get, HttpResponse};
+use actix_web::{get, web, HttpResponse, Responder};
+use log::{error, info};
+use reqwest;
 
 #[get("/")]
 async fn root() -> impl Responder {
     HttpResponse::Ok().body("Hello and welcome to Dragon Bot Z's summon service!")
+}
+
+#[get("/summon")]
+async fn summon(http_client: web::Data<reqwest::Client>) -> impl Responder {
+    let response = http_client.get("http://127.0.0.1:8082/").send().await;
+
+    if let Err(error) = response {
+        error!("[/summon] An error occured: {}", error);
+        return HttpResponse::InternalServerError().body(format!("{error}"));
+    }
+
+    HttpResponse::Ok().body("Ok 👌")
 }
