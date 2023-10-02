75d65bce3e405f940f5029bac937384c42b124d8
Lahène Belhadi <lahcene.belhadi@gmail.com>
2023-10-02 20:45:47 +0200

    fix: Adds call to dbz-portal-service/get-content

--- src/core/api/mod.rs
+++ src/core/api/mod.rs
@@ -2,8 +2,9 @@
 //!
 //! Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
 use actix_web::{get, web, HttpResponse, Responder};
-use log::{error, info};
-use reqwest;
+use dbzlib_rs::model::portal::PortalContent;
+use log::{debug, error};
+use reqwest::Response;
 
 #[get("/")]
 async fn root() -> impl Responder {
@@ -12,12 +13,28 @@ async fn root() -> impl Responder {
 
 #[get("/summon")]
 async fn summon(http_client: web::Data<reqwest::Client>) -> impl Responder {
-    let response = http_client.get("http://dbz-portal-service:8080/").send().await;
+    let response = http_client
+        .get("http://dbz-portal-service:8080/get-content/1")
+        .send()
+        .await;
 
     if let Err(error) = response {
         error!("[/summon] An error occured: {}", error);
         return HttpResponse::InternalServerError().body(format!("{error}"));
     }
+    let response: Response = response.unwrap();
+
+    let content = response.json::<PortalContent>().await;
+    if let Err(error) = content {
+        error!(
+            "[/summon] An error occured while fetching the portal content: {}",
+            error
+        );
+        return HttpResponse::InternalServerError().body(format!("{error}"));
+    }
+    let content: PortalContent = content.unwrap();
+
+    debug!("{:?}", content.characters());
 
     HttpResponse::Ok().body("Ok 👌")
 }
